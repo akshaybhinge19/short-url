256438bb363cab87034094caec8092c507c40e07
Akshay Bhinge <87247305+akshaybhinge19@users.noreply.github.com>
2024-07-10 15:45:52 +0530

    short url initial commit

---
+++ controllers/url.js
@@ -0,0 +1,28 @@
+const URL = require("../models/url")
+const shortid = require('shortid');
+async function handleGenerateNewShortUrl(req,res) {
+    const body = req.body; 
+    if(!body.url) return res.status(400).json("URL is required!");
+    const shortId = shortid.generate();
+    await URL.create({
+        shortId: shortId,
+        redirectURL: body.url,
+        visitHistory: [],
+    })
+    return res.render("home",{
+        id: shortId
+    })
+    // return res.status(200).json({ id:shortId })
+}
+
+async function handleGetAnalytics(req,res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({shortId});
+    return res.json({ totalClicks: result.visitHistory.length, analytics: result.visitHistory })
+}
+
+
+module.exports = {
+    handleGenerateNewShortUrl,
+    handleGetAnalytics
+}
---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { connectMongoDB } = require("./connection")
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/statisRoutes")
+const PORT = 8001;
+const URL = require("./models/url");
+
+// connection
+connectMongoDB("mongodb://127.0.0.1:27017/short-url")
+.then(()=>console.log('MongoDb connected!'))
+.catch((err)=>console.log("error connected to db",err))
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve("./views"))
+
+//middlewares
+// app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+//routes
+app.use("/url",urlRoute);
+app.use("/",staticRoute);
+
+
+// app.get("/test",async (req,res)=>{
+//     const allUrls = await URL.find({});
+//     return res.render("home",{
+//         urls: allUrls
+//     })
+//     // return res.end(`
+//     //     <html>
+//     //     <head></head>
+//     //         <body>
+//     //             <ol>
+//     //                 ${allUrls.map((url)=>`<li>
+//     //                         ${url.shortId} - ${url.redirectURL} - ${url.visitHistory.length}
+//     //                         </li>`).join('')}
+//     //             </ol>
+//     //         </body>
+//     //     </html>
+//     //     `)
+// })
+
+app.get("/url/:shortId", async (req,res)=> {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    },
+    {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now()
+            },
+        }
+    }
+    )
+    // const urlData = await URL.findById(req.params.shortId);
+    res.redirect(entry?.redirectURL);
+})
+
+app.listen(PORT,()=>console.log(`Server Started at PORT: ${PORT}`))
